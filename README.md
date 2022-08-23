0b7290fc38214c5bea22c44f174f1682e004cb0d
Com.Lan <ComlanGiovanni@users.noreply.github.com>
2022-08-23 23:18:14 +0200

    initialisation of file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Philosophers
 Ce projet est une introduction au threading et aux processus, et sur comment travailler sur le même espace mémoire. Vous apprendrez à manipuler des threads. Vous découvrirez les mutex, les sémaphores et la mémoire partagée.
+
+<img align="left" width="100" height="169" src="42_logo.png">
+
+		- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+![main test](https://badgen.net/badge/main/Main-Test/red?icon=github&label)
+![norminette](https://badgen.net/badge/Norminette/42-Norminette-v3-Passed/green?icon=github&label)
+![clean code](https://badgen.net/badge/Readable&Clean-Code/Readable&Clean-Code/blue?icon=github&label)
---
+++ philo/philosophers.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcomlan <gcomlan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 22:55:45 by gcomlan           #+#    #+#             */
+/*   Updated: 2022/08/23 22:57:55 by gcomlan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+
+typedef struct s_ philosophers
+{
+    
+}				t_philosophers;
+
+typedef struct s_argument
+{
+    
+}				t_argument;
+
+#endif
